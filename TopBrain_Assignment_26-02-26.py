308f26d2029235a6ff3bb2fb63e2d29b247df9fd
Vivekshrotriya1 <vivekshrotriyas123@gmail.com>
2026-02-28 12:11:34 +0530

    update

--- TopBrain_Assignment_26-02-26.py
+++ TopBrain_Assignment_26-02-26.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,7 +22,7 @@ print("Learned Regression Equation:")
 print(f"Salary = {m:.3f} × Experience + {b:.3f}")
 
 
-new_experience = [[7]]  
+new_experience = [[7]]
 predicted_salary = model.predict(new_experience)
 
 print("\nPredicted Salary for 7 years experience:")
@@ -40,47 +39,4 @@ plt.plot(x_range, y_line)
 plt.xlabel("Experience (Years)")
 plt.ylabel("Salary (LPA)")
 plt.title("Salary vs Experience (Linear Regression)")
-=======
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-
-
-df = pd.read_csv('salary_lpa.csv')
-
-
-X = df[['Experience_years']]
-y = df['Salary_lpa']
-
-
-model = LinearRegression()
-model.fit(X, y)
-
-
-m = model.coef_[0]
-b = model.intercept_
-
-print("Learned Regression Equation:")
-print(f"Salary = {m:.3f} × Experience + {b:.3f}")
-
-
-new_experience = [[7]]  
-predicted_salary = model.predict(new_experience)
-
-print("\nPredicted Salary for 7 years experience:")
-print(f"{predicted_salary[0]:.2f} LPA")
-
-
-x_range = np.linspace(X.min(), X.max(), 100)
-y_line = model.predict(x_range)
-
-plt.figure()
-plt.scatter(X, y)
-plt.plot(x_range, y_line)
-
-plt.xlabel("Experience (Years)")
-plt.ylabel("Salary (LPA)")
-plt.title("Salary vs Experience (Linear Regression)")
->>>>>>> bebf0ae210bc2e278888349621afcf5d8b49978d
 plt.show()
